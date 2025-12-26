c41f2025d771f4cac670ef77a9db739b8d380aa7
GonuguntaAswini <chowdary3691@gmail.com>
2025-12-26 22:03:23 +0530

    final

--- FirstProject/FirstProject/settings.py
+++ FirstProject/FirstProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#4!e+zpfk@*2106_s%$cllk++4*owq9&k188_gn2nkavg*=xd!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["firstserver-r314.onrender.com","127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
